8a9a0fcd3c398b4e0de7f4ba8128b3624279e2ca
dylan-losey <losey@vt.edu>
2023-09-13 11:04:53 -0400

    results

--- compare/plotter.py
+++ compare/plotter.py
@@ -3,11 +3,11 @@ from scipy.signal import savgol_filter
 import pickle
 import numpy as np
 
-o1 = pickle.load(open("ours/lift/rewards1.pkl", "rb"))
-o2 = pickle.load(open("ours/lift/rewards2.pkl", "rb"))
-o3 = pickle.load(open("ours/lift/rewards3.pkl", "rb"))
-o4 = pickle.load(open("ours/lift/rewards4.pkl", "rb"))
-o5 = pickle.load(open("ours/lift/rewards5.pkl", "rb"))
+o1 = pickle.load(open("ours/lift2/rewards1.pkl", "rb"))
+o2 = pickle.load(open("ours/lift2/rewards2.pkl", "rb"))
+o3 = pickle.load(open("ours/lift2/rewards3.pkl", "rb"))
+o4 = pickle.load(open("ours/lift2/rewards4.pkl", "rb"))
+o5 = pickle.load(open("ours/lift2/rewards5.pkl", "rb"))
 
 s1 = pickle.load(open("sac/lift/rewards1.pkl", "rb"))
 s2 = pickle.load(open("sac/lift/rewards2.pkl", "rb"))
@@ -23,8 +23,8 @@ s10 = pickle.load(open("sac/lift/rewards10.pkl", "rb"))
 ours = np.array([o1, o2, o3, o4, o5])
 sac = np.array([s1, s2, s3, s4, s5, s6, s7, s8, s9, s10])
 
-mo, so = np.mean(ours, axis=0), np.std(ours, axis=0) / np.sqrt(5)
-ms, ss = np.mean(sac, axis=0), np.std(sac, axis=0) / np.sqrt(10)
+mo, so = np.mean(ours, axis=0), np.std(ours, axis=0) / np.sqrt(len(ours))
+ms, ss = np.mean(sac, axis=0), np.std(sac, axis=0) / np.sqrt(len(sac))
 mo = savgol_filter(mo, 10, 2)
 ms = savgol_filter(ms, 10, 2)
 
@@ -34,3 +34,7 @@ plt.fill_between(x, mo+so, mo-so, alpha=0.2)
 plt.plot(x, ms, 'k--')
 plt.plot(x, mo, 'b-')
 plt.show()
+
+
+# collab@192.168.1.18
+# collab@172.29.29.124
