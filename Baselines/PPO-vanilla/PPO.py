8362e38f6b81407e9c89975dfa2a501305756440
dylan-losey <losey@vt.edu>
2023-12-12 13:42:04 -0500

    first completed draft of baselines

--- Baselines/PPO-vanilla/PPO.py
+++ Baselines/PPO-vanilla/PPO.py
@@ -7,12 +7,12 @@ from torch.distributions import Categorical
 print("============================================================================================")
 # set device to cpu or cuda
 device = torch.device('cpu')
-# if(torch.cuda.is_available()): 
-#     device = torch.device('cuda:0') 
-#     torch.cuda.empty_cache()
-#     print("Device set to : " + str(torch.cuda.get_device_name(device)))
-# else:
-#     print("Device set to : cpu")
+if(torch.cuda.is_available()): 
+    device = torch.device('cuda:0') 
+    torch.cuda.empty_cache()
+    print("Device set to : " + str(torch.cuda.get_device_name(device)))
+else:
+    print("Device set to : cpu")
 print("============================================================================================")
 
 
